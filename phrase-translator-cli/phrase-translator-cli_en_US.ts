782c30d1a655b71f339f52d1282d9b862409e991
Egor Nikulenkov <enikulenkov@gmail.com>
2021-01-04 23:15:04 +0000

    Use CMake instead of qmake

--- phrase-translator-cli/phrase-translator-cli_en_US.ts
+++ phrase-translator-cli/phrase-translator-cli_en_US.ts
@@ -1,3 +1,4 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="phrase-translator-cli_en_US"></TS>
+<TS version="2.1" language="phrase-translator-cli_en_US">
+</TS>
